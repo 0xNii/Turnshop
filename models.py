82a5a0ba632a00caa6b772cfc35e42cf502d2e17
localkid63 <43614381+localkid63@users.noreply.github.com>
2019-09-05 12:54:06 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+class Book(models.Model):
+    img = models.ImageField(upload_to="gallery")
+    title = models.TextField()
+    desc = models.TextField()
+    price = models.FloatField()
+    def __str__(self):
+        return self.title  
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete= models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')      
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+class SignUp(models.Model):
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email        
+
+    
+              
--- views.py
+++ views.py
@@ -1,33 +1,121 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from .models import Book,Profile,SignUp
+from .forms import ImageForm,EmailSignUpForm
+from django.conf import settings
+from django.http import HttpResponseRedirect  
+import json
+import requests
 
 # Create your views here.
-def register(request):
+def home(request):
+    books = Book.objects.all()
+    return render(request,"Turnshop.html",{'books': books})
 
+def login(request):
+     if request.method == 'POST':
+         username = request.POST['username']
+         password = request.POST['password']
+
+         user = auth.authenticate(username=username,password=password)
+
+         if user is not None:
+             auth.login(request,user)
+             return redirect('/')
+         else:
+             messages.info(request,"Invalid username or password.")
+             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+     else:
+         return render(request,'login.html')        
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/') 
+
+def updateInfo(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
+        email = request.POST['em_field']
         username = request.POST['username']
-        n_password = request.POST['n_password']
-        c_password = request.POST['c_password']
-        
-        if n_password == c_password:
-            if User.objects.filter(username=username).exists():
-                messages.warning(request,"The username already exists")
-                return redirect('register')
+        user = request.user
+        form = ImageForm(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+      
+        if user.username == username:
+            if user.email == email:
+                return redirect('edit')
             else:
                 if User.objects.filter(email=email).exists():
-                    messages.warning(request,"This email has already been used by another account")
-                    return redirect('register')
+                    messages.info(request,"This email has already been used by another account.")
+                    return redirect('edit')
                 else:
-                    user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=n_password,email=email)
-                    user.save()    
-                    return redirect('register')            
-        else:
-            messages.warning(request,"The passwords entered do not match")
-            return redirect('register')
+                    user.email = email
+                    user.save()
+                    messages.info(request,"Your account has been updated successfully!")
+                    return redirect('edit') 
+        
+        else: 
+            if user.email == email:
+                if User.objects.filter(username=username).exists():
+                    messages.info(request,"The username already exists.")
+                    return redirect('edit')
+                else:
+                    user.username = username
+                    user.save()
+                    messages.info(request,"Your account has been updated successfully!") 
+                    return redirect('edit')
+            else:
+                if User.objects.filter(username=username).exists():
+                    messages.info(request,"The username already exists")
+                    return redirect('edit')
+                else:
+                    if User.objects.filter(email=email).exists():
+                        messages.info(request,"This email has already been used by another account.")
+                        return redirect('edit')
+                    else:
+                        user.username = username
+                        user.email = email
+                        user.save()   
+                        messages.info(request,"Your account has been updated successfully!") 
+                        return redirect('edit')     
+         
 
-    else:
-        return render(request,'Register.html') 
+       
+
+def edit(request): 
+    return render(request,"updateInfo.html")
+
+MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
+MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
+MAILCHIMP_EMAIL_AUDIENCE_ID = settings.MAILCHIMP_EMAIL_AUDIENCE_ID
+
+url_api = f'https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0'
+members_endpoint = f'{url_api}/lists/{MAILCHIMP_EMAIL_AUDIENCE_ID}/members'
+
+def inscribeToAudience(email):
+    data = {
+        "email_address": email,
+        "status": "subscribed", 
+    }
+    r = requests.post(
+        members_endpoint,
+        auth=('',MAILCHIMP_API_KEY),
+        data=json.dumps(data)
+    )
+    return r.status_code,r.json()
+
+def subscribe(request):
+    email = request.POST['email']
+    form = EmailSignUpForm(request.POST) 
+    if request.method == 'POST':
+        if form.is_valid():
+            #check if user has already subscribed to newsletters
+            queryset = SignUp.objects.filter(email=email)
+            if queryset.exists():
+                #messages.info(request,"You have already subscribe to our newsletters")
+                pass
+            else:
+                inscribeToAudience(email)
+                form.save()    
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))    
